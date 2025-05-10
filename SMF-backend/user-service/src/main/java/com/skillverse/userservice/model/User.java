731bd5d73715fc553b389020b225340077782b5f
jeakhtar <jeakhtar@LIN-77F8MW3>
2025-05-10 16:54:17 +0530

    Added Lombok and removed getter and setter functions

--- SMF-backend/user-service/src/main/java/com/skillverse/userservice/model/User.java
+++ SMF-backend/user-service/src/main/java/com/skillverse/userservice/model/User.java
@@ -5,8 +5,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
 @Document(collection = "users")
+@Data
 public class User {
 
    @Id
@@ -24,23 +26,4 @@ public class User {
    private String bio;
    private String profilePictureUrl;
 
-   // getters & setters
-
-   public String getId() { return id; }
-   public void setId(String id) { this.id = id; }
-
-   public String getUsername() { return username; }
-   public void setUsername(String username) { this.username = username; }
-
-   public String getEmail() { return email; }
-   public void setEmail(String email) { this.email = email; }
-
-   public String getPassword() { return password; }
-   public void setPassword(String password) { this.password = password; }
-
-   public String getBio() { return bio; }
-   public void setBio(String bio) { this.bio = bio; }
-
-   public String getProfilePictureUrl() { return profilePictureUrl; }
-   public void setProfilePictureUrl(String profilePictureUrl) { this.profilePictureUrl = profilePictureUrl; }
 }
